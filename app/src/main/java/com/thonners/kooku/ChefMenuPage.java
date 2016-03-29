350ccf4bd96dfb6cd018f070f3f33f87737f82a5
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2016-03-29 23:42:39 +0100

    Stop keyboard from popping up and add ASAP & location buttons to search page.
    - Modify AndroidManifest to ensure keyboard remains hidden when SearchActivity starts.
    - Add ASAP and Location buttons to activity_search.xml.
    - Set onClick for each button and create the necessary methods in the Java. Prints a log message acknowledging the click.

---
+++ app/src/main/java/com/thonners/kooku/ChefMenuPage.java
@@ -0,0 +1,19 @@
+package com.thonners.kooku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Activity to display a chef's menu
+ *
+ * @author M Thomas
+ * @since 25/03/16
+ */
+public class ChefMenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_menu_page);
+    }
+}
--- app/src/main/java/com/thonners/kooku/SearchActivity.java
+++ app/src/main/java/com/thonners/kooku/SearchActivity.java
@@ -5,27 +5,47 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 
+/**
+ * Activity to get and display the results of the available chefs.
+ *
+ * @author M Thomas
+ * @since 25/03/16
+ */
 public class SearchActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "SearchActivity";
 
     private RecyclerView recyclerView ;
     private RecyclerView.Adapter rcAdapter;
     private RecyclerView.LayoutManager rcLayoutManager;
 
+    /**
+     * Standard onCreate override.
+     * Sets up the RecyclerView with a layout manager and SearchResultsRVAdapter.
+     * Currently populates results with fakes for the purposes of demonstration. TODO: Get proper results and delete this line!
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the layout
         setContentView(R.layout.activity_search);
-
+        // Stop the keyboard from popping up
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        // Get the instance of the view
         recyclerView = (RecyclerView) findViewById(R.id.search_results_recycler_view) ;
 
-        // use this setting to improve performance given that changes
+        // Use this setting to improve performance given that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
+        // Use a linear layout manager
         rcLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(rcLayoutManager);
 
@@ -35,6 +55,23 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Method to search for the chef who could deliver the food in the soonest possible time.
+     * Method will be called when the user clicks on the card.
+     * @param view The instance of the CardView which is the ASAP card.
+     */
+    public void asapClicked(View view) {
+        Log.d(LOG_TAG, "A.S.A.P. button clicked.");
+    }
+
+    /**
+     * Method to search for chefs based on the user's current location
+     * Method will be called when the user clicks on the card.
+     * @param view The instance of the CardView which is the location card.
+     */
+    public void locationClicked(View view) {
+        Log.d(LOG_TAG, "Location button clicked.");
+    }
     /**
      * Method to begin the search for results.
      * In practice, will call an AsyncTask to find the results, then populat the view.
